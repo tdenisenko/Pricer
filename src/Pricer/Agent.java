9e638a54940bbb8fc0ceecdcd921eabf223894fd
Timothy Denisenko <timothy243@gmail.com>
2013-09-18 19:14:58 +0300

    Runnable Agents

--- src/Pricer/Agent.java
+++ src/Pricer/Agent.java
@@ -1,5 +1,17 @@
 package Pricer;
 
-public class Agents {
+public class Agent implements Runnable {
+	
+	static int AGENT_COUNT;
+	
+	Agent() {
+		AGENT_COUNT++;
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		OrderBook o;
+	}	
 
 }
