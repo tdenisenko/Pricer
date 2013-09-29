b43feae002b3db15ecc6276ac6378526a1d69bc0
Timothy Denisenko <timothy243@gmail.com>
2013-09-29 04:47:21 +0300

    Agent database added

--- src/Pricer/Agent.java
+++ src/Pricer/Agent.java
@@ -5,12 +5,32 @@ package Pricer;
  */
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Random;
 
 public class Agent implements Runnable {
 
 	static int AGENT_COUNT;
 	static Random r = new Random();
+
+	Comparator<Pricer> comparatorOrders = new Comparator<Pricer>() {
+		public int compare(Pricer p1, Pricer p2) {
+			return (int) (p2.timestamp - p1.timestamp);
+		}
+	};
+
+	// The Order List (always sorted and synchronized)
+	List<Pricer> listOrders = Collections
+			.synchronizedList(new ArrayList<Pricer>() {
+				public boolean add(Pricer p) {
+					super.add(p);
+					Collections.sort(listOrders, comparatorOrders);
+					return true;
+				}
+			});
 
 	Agent() {
 		AGENT_COUNT++;
@@ -22,23 +42,23 @@ public class Agent implements Runnable {
 		while (true) {
 			String a = "";
 			String message = "L";
-			String side = "";
+			char side;
 			// 50% chance for Bid and Ask
 			if (Math.random() < 0.5) {
-				side += "B";
+				side = 'B';
 			} else {
-				side += "S";
+				side = 'S';
 			}
 			double price;
 			// mean = current price
-			if (side.equals("B")) {
+			if (side == 'B') {
 				price = OrderBook.listBuy.get(0).price;
 			} else {
 				price = OrderBook.listSell.get(OrderBook.listSell.size() - 1).price;
 			}
 			// std. dev. = 1
 			price += r.nextGaussian();
-			int temp = (int)(price * 100);
+			int temp = (int) (price * 100);
 			price = temp / 100.0;
 			// mean = 100
 			int size = 100;
@@ -54,6 +74,8 @@ public class Agent implements Runnable {
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
+			} finally {
+				listOrders.add(new Pricer(message, side, price, size));
 			}
 			long millis = ((long) (r.nextGaussian() * 1000)) + 5000;
 			try {
